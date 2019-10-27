34700cbc2523b31019096150688bae0b3592d18b
Akanksha Pandey <akanksha.pandey@suiit.ac.in>
2019-10-27 19:32:21 +0530

    Update register.php

--- wt-assignment/register.php
+++ wt-assignment/register.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Login | signup</title>
+    <title>Login | signupz</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Anime Manga Comic | HTML CSS3 BS4 JS jQuery PHP MySQLi">
@@ -125,7 +125,7 @@
 				    <!-- Terms of service -->
 				    <p>By clicking
 				        <em>Sign up</em> you agree to our
-				        <a href="" target="">terms of service</a>
+				        <a href="#" target="#">terms of service</a>
 				    </p>
 					</form>
 				<!-- Default form signup -->
